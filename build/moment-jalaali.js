bb65577b33cfbe5bea52168ee0e0bc0628787bc7
Ehsan Danesh <ehsandanesh@Ehsans-MacBook-Pro.local>
2016-01-30 22:53:30 +0330

    adding isSame to jMoment to find out if two moments are in same unit

--- build/moment-jalaali.js
+++ build/moment-jalaali.js
@@ -988,6 +988,30 @@ jMoment.fn.subtract = function (val, units) {
   }
   return this
 }
+jMoment.fn.isSame  = function (input, units) {
+  var before;
+  var after;
+  var middle;
+  units = normalizeUnits(units);
+  if (units === 'jyear' || units === 'jmonth') {
+    units = units.replace(/j/g,"");
+    units = normalizeUnits(units || 'millisecond');
+    units = "j" + units;
+    before = this.clone().startOf(units).valueOf();
+    after = this.clone().endOf(units).valueOf();
+    middle = input.clone().valueOf();
+    if ( before <= middle && after >= middle)
+    {
+      return true;
+    }
+    return false;
+  }
+  else
+  {
+    units = units.replace(/j/g,"");
+    return moment.fn.isSame.call(this,input,units);
+  } 
+}
 
 jMoment.fn.startOf = function (units) {
   units = normalizeUnits(units)
@@ -1002,6 +1026,7 @@ jMoment.fn.startOf = function (units) {
     this.milliseconds(0)
     return this
   } else {
+    units = units.replace(/j/g,"");
     return moment.fn.startOf.call(this, units)
   }
 }
